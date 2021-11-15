ef410eabacc7967d0a63394a836fb267755010cb
MrGullano <90940844+MrGullano@users.noreply.github.com>
2021-11-15 04:57:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const client = new Client({
 module.exports = client;
 
 // Global Variables
-client.commands = new Collection();
-client.slashCommands = new Collection();
+client.commands = new Collection(-);
+client.slashCommands = new Collection(-);
 client.config = require("./config.json");
 
 // Initializing the project
 require("./handler")(client);
 
-client.login(client.config.token);
+client.login(ODg1MTc1Mjg4ODk0NDY0MDUy.YTjNug.PX8G2AQKHaEivga8XUWKWkAqMws);
